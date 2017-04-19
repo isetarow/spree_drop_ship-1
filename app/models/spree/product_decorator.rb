0b4a771e888577afede65b624b645e51574457b9
Yad Faeq <yad.faeq@gmail.com>
2017-04-19 14:34:22 -0700

    byebug removed

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -19,14 +19,12 @@ Spree::Product.class_eval do
     suppliers.present?
   end
 
-require "byebug"
   private
 
   def populate_for_supplier!(supplier)
     variants_including_master.each do |variant|
       unless variant.suppliers.pluck(:id).include?(supplier.id)
         variant.suppliers << supplier
-        # byebug
         supplier.stock_locations.each { |location| location.set_up_stock_item(variant) }
       end
     end
